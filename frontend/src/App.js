da61a34d8a0c6abb391dfa49f3723e6bb45ced0b
Ahmed Alaa <1808283@eng.asu.edu.eg>
2024-12-31 18:18:58 +0200

    Added routes for new pages
    Routes for EcuFlashingPage, RemoteTuningPage, DynoTuningPage, ContactPage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home";
 import PreviousWorkPage from "./pages/PreviousWork";
+import EcuFlashingServicesPage from "./pages/EcuFlashingServices";
+import RemoteTuningPage from "./pages/RemoteTuning";
+import DynoTuningPage from "./pages/DynoTuning";
+import ContactPage from "./pages/Contact";
 
 import Layout from "./components/layout/Layout";
 
@@ -10,6 +14,13 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/previous-work" element={<PreviousWorkPage />} />
+        <Route
+          path="/ecu-flashing-services"
+          element={<EcuFlashingServicesPage />}
+        />
+        <Route path="/remote-tuning" element={<RemoteTuningPage />} />
+        <Route path="/dyno-tuning" element={<DynoTuningPage />} />
+        <Route path="/contact" element={<ContactPage />} />
       </Routes>
     </Layout>
   );
