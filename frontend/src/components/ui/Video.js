742c1bdb723e893e1015162a99dffc6afef043c2
Ahmed Alaa <1808283@eng.asu.edu.eg>
2025-01-08 09:12:43 +0200

    Created CSS file for Video component

--- frontend/src/components/ui/Video.js
+++ frontend/src/components/ui/Video.js
@@ -1,7 +1,9 @@
+import classes from "./Video.module.css";
+
 export default function Video({ youtubeID }) {
   return (
     <iframe
-      className="video"
+      className={classes.video}
       title="Youtube player"
       sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
       src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
