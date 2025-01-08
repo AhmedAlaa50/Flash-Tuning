1da78340b3c0d27ec387b299755beec789a94715
Ahmed Alaa <1808283@eng.asu.edu.eg>
2025-01-08 08:53:32 +0200

    Created Video component
    Works as an embedded video in the website

---
+++ frontend/src/components/ui/Video.js
@@ -0,0 +1,10 @@
+export default function Video({ youtubeID }) {
+  return (
+    <iframe
+      className="video"
+      title="Youtube player"
+      sandbox="allow-same-origin allow-forms allow-popups allow-scripts allow-presentation"
+      src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
+    ></iframe>
+  );
+}
